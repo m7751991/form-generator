57e1dc56f112805cde96d890bfd60c88c6e37700
m7751991 <m7751991@126.com>
2022-08-01 13:10:06 +0800

    ELEMENT

---
+++ src/components/commonui/headerComponents.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="fd-xt-header">
+    {{ text }}
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      default: "头部数据",
+    },
+    name:''
+  },
+  created(){
+    console.log(this.$props,'texttext');
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.fd-xt-header {
+  border: 1px solid rgb(75, 165, 255);
+  box-shadow: 1px 2px 1px 1px;
+  border-radius: 5px;
+}
+</style>
---
+++ src/views/index/main.js
@@ -0,0 +1,20 @@
+import './public-path';
+import Vue from 'vue'
+import App from './App.vue'
+import router from '@/router'
+import '@/styles/index.scss'
+import '@/icons'
+import axios from 'axios'
+import Tinymce from '@/components/tinymce/index.vue'
+import headerComponents from '@/components/commonui/headerComponents'
+
+Vue.component('headerComponents', headerComponents)   
+Vue.component('tinymce', Tinymce)
+
+Vue.config.productionTip = false
+Vue.prototype.$axios = axios
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
